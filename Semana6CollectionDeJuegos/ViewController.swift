baca1294ea94392c32fc5e5af61aa705d8d662a8
yamil564 <sas523363.fsf@hotmail.com>
2021-04-15 10:25:56 -0500

    primer commit

---
+++ Semana6CollectionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Semana6CollectionDeJuegos
+//
+//  Created by mbtec22 on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
